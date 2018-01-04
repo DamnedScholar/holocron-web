aa615752f93142bdbb4ee5b3ef8edb2b16ee07e1
DamnedScholar <stickfigureonfire@gmail.com>
2018-01-04 13:53:19 -0600

    So it begins.

---
+++ src/components/pages/HomePage/index.js
@@ -0,0 +1,12 @@
+// https://github.com/diegohaz/arc/wiki/Atomic-Design
+import React from 'react'
+import {Menu} from "semantic-ui-react";
+import { Houndify } from 'houndify'
+
+const HomePage = () => {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+export default HomePage
